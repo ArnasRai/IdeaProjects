3ed3781fb71d5c8917b9830a977621778839f206
Arnas.Rai <arnas.rai@gmail.com>
2020-08-30 10:46:30 +0300

    local changes 2020 08 30 (part 2 )

---
+++ src/main/java/lt/sdacademy/university/converters/PersonNameConverter.java
@@ -0,0 +1,18 @@
+package lt.sdacademy.university.converters;
+
+import lt.sdacademy.university.models.entities.PersonEntity;
+
+/*public class PersonNameConverter extends AbstractConverter<PersonEntity, PersonNameConverter> {
+
+    public personName convert(PersonEntity person) {
+
+
+        personName result = new personName();
+        result.setId(person.getId());
+        String fullName = person.getName() + " " + person.getSurname();
+        result.setFullName(fullName);
+
+
+        return result;
+    }
+}*/
---
+++ src/main/java/lt/sdacademy/university/models/dto/PersonName.java
@@ -0,0 +1,4 @@
+package lt.sdacademy.university.models.dto;
+
+public class PersonName {
+}
